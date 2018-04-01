40980dc38e5c6b6196a0e14bf83fb303c97028cc
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-01 05:44:59 -0400

    stuff

--- app/pods/components/chart-card/component.js
+++ app/pods/components/chart-card/component.js
@@ -3,18 +3,19 @@ import Component from '@ember/component';
 
 export default Component.extend({
     chartData: Ember.computed('data', function() {
-        // debugger
+        debugger
         // let data = this.get('data')
-        // for(let i = 0; i< data.default[0].value.length;i++ ){
-        //     for(let j = 0; j < data.default.length; j++){
-        //         data.default[j].value[i]
+        // let arr = []
+        // for(let i = 0; i< data.labels.length;i++ ){
+        //     for(let j = 0; j < data.data.default.length; j++){
+        //         arr.push(data.data.default[j].value[i])
         //     }
         // }
         // let data = this.get('data').default.map((x) => {
         //     return this.prefix + x;
         // })
         // this.get('data').default.timelineData.map((x) => {
-	
+        return 0
         // })
     })
 });
